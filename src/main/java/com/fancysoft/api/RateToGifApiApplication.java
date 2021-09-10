dede441e79574b187a8a699b66b6a84945aabe63
aleksandr.popov@t-systems.com <aleksandr.popov@t-systems.com>
2021-09-10 17:55:52 +0300

    initial commit - v1

---
+++ src/main/java/com/fancysoft/api/RateToGifApiApplication.java
@@ -0,0 +1,13 @@
+package com.fancysoft.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateToGifApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RateToGifApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/fancysoft/api/RateToGifApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fancysoft.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateToGifApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
